60cd5b6919d67707e5b799e5f6a9f234f026985f
Parna Salian <parna146@gmail.com>
2021-12-26 12:47:49 +0000

    adding login controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('auth/login', 'Auth\LoginController@getLogin');
+Auth::routes();
+Auth::routes(['verify' => true]);
