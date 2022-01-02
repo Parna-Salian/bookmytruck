ded4bb0824439b16f04fea5517f37bde7b6732a8
Parna Salian <parna146@gmail.com>
2022-01-02 10:31:48 +0000

    Committing initial home page changes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('layouts.app');
-});
-
-Auth::routes();
-//Auth::routes(['verify' => true]);
-
-
-Route::view('/index', 'home');
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Auth::routes();
+    //Auth::routes(['verify' => true]);
+    Route::get('/home', 'HomeController@index')->name('home');
+    
+    //Route::view('/index', 'home');
+ 
